b9208954f50ad1bfd8a9f3d4e58e2f74c0a964f6
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-26 00:45:47 +0100

    Update margin-top in hero section, so that the nav-bar doesn't push it down

--- src/components/home/sections/hero.tsx
+++ src/components/home/sections/hero.tsx
@@ -3,7 +3,7 @@ import Logo from '../../../assets/einfacheinfach_logo_blue.svg';
 
 const Hero = () => {
     return (
-        <div className="h-lvh flex justify-center items-center">
+        <div className="h-lvh flex justify-center items-center -mt-20">
             <img className="h-20" src={Logo}></img>
         </div>
     )
