13547003aa2c7f8bb2b8569593d053b77b88ca02
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-26 00:53:21 +0100

    Add Instagram logo to footer and make text of p bigger

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,14 +1,20 @@
 import React from "react";
+import InstagramLogo from '../../assets/icons/instagram_logo.png';
 
 const Footer = () => {
     return (
         <div className="h-[500px] w-full p-20 flex flex-col justify-between text-white bg-primary">
-            <div>
-                <h1>Contact</h1>
+            <div className="flex flex-col gap-10">
+                <h1 className="font-bold">Contact</h1>
                 <p className="px-10">Lorem ipsum dolor sit amet.</p>
             </div>
-            <div>
-                <a href="https://www.instagram.com/einfacheinfach.studio/">einfacheinfach.studio</a>
+            <div className="w-full flex flex-row justify-end">
+                <a href="https://www.instagram.com/einfacheinfach.studio/">
+                    <div className="flex flex-row gap-4 items-center">
+                        <img className="h-10" src={InstagramLogo} alt="instagram-logo" />
+                        einfacheinfach.studio
+                    </div>
+                </a>
             </div>
         </div>
     )
