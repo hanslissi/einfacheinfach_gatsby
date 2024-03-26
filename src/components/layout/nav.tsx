41541b80d4a2c169fadfcd808c5355b8878547c1
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-26 10:52:42 +0100

    mobile ui: Update navigation styling

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Nav = () => {
     return (
-        <header className="sticky top-0 z-50 h-20">
-            <nav className="h-full flex flex-row gap-8 justify-end items-center text-secondary px-16">
+        <header className="sticky h-20 top-0 bg-beige z-50">
+            <nav className="h-full flex text-secondary flex-row justify-between items-center px-8 md:px-16 md:gap-8 md:justify-end">
                 <a href="#services"><span>services</span></a>
                 <a href="#work"><span>work</span></a>
                 <a href="#about"><span>about us</span></a>
