c2a725808899970b8e880f21ed0a8ee3b67980b9
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-26 13:12:08 +0100

    mobile ui: update footer component styles

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,10 +3,10 @@ import InstagramLogo from '../../assets/icons/instagram_logo.png';
 
 const Footer = () => {
     return (
-        <div className="h-[500px] w-full p-20 flex flex-col justify-between text-white bg-primary">
-            <div className="flex flex-col gap-10">
+        <div className="h-[300px] w-full px-4 py-10 flex flex-col justify-between text-white bg-primary md:p-20 md:h-[500px]">
+            <div className="flex flex-col gap-4 md:gap-10">
                 <h1 className="font-bold" id="contact">Contact</h1>
-                <p className="px-10">Lorem ipsum dolor sit amet.</p>
+                <p className="md:px-10">Lorem ipsum dolor sit amet.</p>
             </div>
             <div className="w-full flex flex-row justify-end">
                 <a href="https://www.instagram.com/einfacheinfach.studio/">
