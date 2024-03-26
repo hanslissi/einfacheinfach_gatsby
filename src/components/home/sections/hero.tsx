f1badf01d6f8d22495f778e5ddc5ef76e3360299
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-26 11:00:27 +0100

    mobile ui: Update logo size in hero section

--- src/components/home/sections/hero.tsx
+++ src/components/home/sections/hero.tsx
@@ -4,7 +4,7 @@ import Logo from '../../../assets/einfacheinfach_logo_blue.svg';
 const Hero = () => {
     return (
         <div className="h-lvh flex justify-center items-center -mt-20">
-            <img className="h-20" src={Logo}></img>
+            <img className="h-12 md:h-20" src={Logo}></img>
         </div>
     )
 }
