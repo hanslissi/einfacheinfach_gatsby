8f526cbfceb878822849077bff263784ca98e7cc
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-26 12:53:17 +0100

    mobile ui: update service section shitty and add clsx dependency

--- src/components/home/icon-information-card.tsx
+++ src/components/home/icon-information-card.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import clsx from "clsx";
 
 interface IconInformationCardProps {
     title: string;
     description: string;
     icon: any;
+    className?: string; // Add className prop
 }
 
-const IconInformationCard = ({ title, description, icon }: IconInformationCardProps) => {
+const IconInformationCard = ({ title, description, icon, className }: IconInformationCardProps) => {
     return (
-        <div className="w-full h-full p-4 border rounded-xl flex flex-col gap-6">
+        <div className={clsx("w-full h-full mx-auto max-w-[275px] p-2 border rounded-xl flex flex-col gap-6 md:p-4", className)}>
             <img src={icon} className="h-[40%]" />
             <h2 className="font-bold text-primary text-center">{title}</h2>
             <p>{description}</p>
--- src/components/home/sections/services.tsx
+++ src/components/home/sections/services.tsx
@@ -9,14 +9,14 @@ import UnderlineScribbleBigSvg from "../../../assets/underline_scribble_big.svg"
 const Services = () => {
     return (
         <SectionWrapper>
-            <div className="flex flex-col gap-32 items-center">
+            <div className="flex flex-col items-center gap-16 md:32">
                 <h1 id="services">Wir erklären - <br />
                     aber <span className="relative inline-block">
                         einfach einfach!
                         <img src={UnderlineScribbleBigSvg} className="absolute -bottom-[80%] left-0" />
                     </span>
                 </h1>
-                <div className="w-full h-[400px] flex flex-row justify-center gap-32 px-60">
+                <div className="w-full grid grid-cols-2 gap-6 md:grid-cols-3 md:gap-12 md:grid-cols-3">
                     <IconInformationCard
                         title="Frage"
                         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut?"
