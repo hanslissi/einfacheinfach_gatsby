f64556cc7cb7bfc7b0589cf7d03761e2bc82218d
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-03-29 11:22:29 +0100

    Remove unused useState import

--- src/components/char-spreader-span.tsx
+++ src/components/char-spreader-span.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Variants, motion, useAnimation } from 'framer-motion';
 
 interface CharSpreaderSpanProps {
