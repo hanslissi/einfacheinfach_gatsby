34cddfc7e4d6388210d4b5e43b173c62324e0971
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-04-04 14:24:40 +0200

    add solid error handling of form

--- src/components/hire-us/contact-form.tsx
+++ src/components/hire-us/contact-form.tsx
@@ -5,6 +5,10 @@ import SectionWrapper from "../wrappers/section-wrapper";
 import TextArea from "../commons/text-area";
 import emailjs from "@emailjs/browser"
 import InfinityLoopArrow from "../animated-commons/infinity-loop-arrow";
+import ReCAPTCHA from 'react-google-recaptcha';
+import Modal from "../commons/modal";
+import { AnimatePresence } from "framer-motion";
+import InfoMessage from "../commons/info-message";
 
 const emailServiceId = process.env.EMAILJS_SERVICE_ID || ''; // Ensure process.env.EMAILJS_SERVICE_ID is defined
 const emailTemplateId = process.env.EMAILJS_CONTACT_TEMPLATE_ID || ''; // Ensure process.env.EMAILJS_CONTACT_TEMPLATE_ID is defined
@@ -26,15 +30,18 @@ const inputValidation = {
 
 const ContactForm = () => {
     const [loading, setLoading] = useState(false);
+    const [showRecaptcha, setShowRecaptcha] = useState(false);
+    const [emailStatus, setEmailStatus] = useState<'idle' | 'success' | 'error'>('idle');
     const [loadingAnimationPlaying, setLoadingAnimationPlaying] = useState(false); // Cause I want the loading animation to finish playing even after it's done loading
-    const methods = useForm();
+    const methods = useForm<ContactFormData>();
 
-    const sendEmail = (data: ContactFormData) => {
+    const sendEmail = (data: ContactFormData, reCaptchaToken: string) => {
         const templateParams = {
             from_company: data.fromCompany,
             work_field: data.workField,
             contact_email: data.contactEmail,
             message: data.message,
+            'g-recaptcha-response': reCaptchaToken,
         }
 
         setLoading(true);
@@ -44,7 +51,6 @@ const ContactForm = () => {
             publicKey: emailjsPublicKey,
         }).then(
             (response) => {
-                console.log(response);
                 setLoading(false);
                 methods.reset({
                     fromCompany: '',
@@ -52,21 +58,44 @@ const ContactForm = () => {
                     contactEmail: '',
                     message: '',
                 });
+                setEmailStatus('success');
             },
             (err) => {
-                console.log(err);
                 setLoading(false);
+                setEmailStatus('error');
             },
         );
     }
 
-    const handleClickButton = methods.handleSubmit((data) => {
-        sendEmail(data as ContactFormData);
-    })
+    const handleRecaptchaChange = (value: string | null) => {
+        if (value) {
+            setShowRecaptcha(false);
+            sendEmail(methods.getValues(), value);
+        }
+    }
+
+    const handleClickButton = () => {
+        setEmailStatus('idle');
+        methods.handleSubmit(() => {
+            // form is valid
+            setShowRecaptcha(true);
+        })();
+    };
 
     const handleAnimationComplete = () => {
         if (!loading) {
             setLoadingAnimationPlaying(false);
+        }
+    }
+
+    const getInfoMessage = () => {
+        switch (emailStatus) {
+            case 'success':
+                return 'Message sent successfully';
+            case 'error':
+                return 'An error occurred while sending the message please try again later';
+            default:
+                return '';
         }
     }
 
@@ -107,10 +136,10 @@ const ContactForm = () => {
                                 placeholder="describe to us how we can help your company"
                                 validation={inputValidation}
                             />
-                            <div className="relative w-full">
+                            <div className="relative w-full h-32 my-20">
                                 {loadingAnimationPlaying &&
                                     <InfinityLoopArrow
-                                        className="absolute w-full left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-110 -rotate-6 origin-center z-10"
+                                        className="absolute w-full left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-110 -rotate-6 origin-center"
                                         duration={2}
                                         loop={loadingAnimationPlaying}
                                         onAnimationComplete={handleAnimationComplete}
@@ -119,10 +148,33 @@ const ContactForm = () => {
                                 <button
                                     onClick={handleClickButton}
                                     disabled={loading}
-                                    className="relative w-full bg-primary text-white px-20 py-10 my-20 rounded-3xl text-4xl z-20"
+                                    className="relative w-full h-full bg-primary text-white rounded-3xl text-4xl"
                                 >
                                     Send Request
                                 </button>
+                                <div className="flex flex-col items-center mt-4 min-h-8">
+                                    <AnimatePresence initial={false} mode="wait">
+                                        {(emailStatus !== 'idle' && !loadingAnimationPlaying) && (
+                                            <InfoMessage
+                                                message={getInfoMessage()}
+                                                type={emailStatus === 'success' ? 'success' : 'error'}
+                                            />
+                                        )}
+                                    </AnimatePresence>
+                                </div>
+                                <Modal open={showRecaptcha} onClose={() => setShowRecaptcha(false)} >
+                                    <div className="flex flex-col items-center gap-4">
+                                        Please solve the reCAPTCHA to send your request
+                                        <ReCAPTCHA
+                                            className="inline-block"
+                                            sitekey={process.env.RECAPTCHA_SITE_KEY || ''}
+                                            onChange={handleRecaptchaChange}
+                                        />
+                                        <button className="underline" onClick={() => setShowRecaptcha(false)}>
+                                            Cancel message request
+                                        </button>
+                                    </div>
+                                </Modal>
                             </div>
                         </div>
                     </form>
