6b5611d0895fabb3366ccff5811a7e3e6753ed08
Johannes Riedmüller <riedmueller.jo@gmail.com>
2024-04-05 09:27:00 +0200

    actually fix sizing issue for IconInformationCards

--- src/components/home/icon-information-card.tsx
+++ src/components/home/icon-information-card.tsx
@@ -11,10 +11,10 @@ interface IconInformationCardProps {
 
 const IconInformationCard = ({ title, description, icon, children, className }: IconInformationCardProps) => {
     return (
-        <div className="relative w-full mx-auto max-w-[275px] max-h-fit">
+        <div className="relative w-full h-full mx-auto max-w-[275px]">
             {children}
-            <div className={clsx("relative w-full h-full bg-beige p-2 border rounded-xl flex flex-col gap-6 md:p-4", className)}>
-                <img src={icon} className="h-[40%]" />
+            <div className={clsx("relative w-full max-h-fit bg-beige p-2 border rounded-xl flex flex-col gap-6 md:h-full md:p-4", className)}>
+                <img src={icon} className="px-10 md:px-0 w-full" />
                 <h2 className="font-bold text-primary text-center">{title}</h2>
                 <p>{description}</p>
             </div>
